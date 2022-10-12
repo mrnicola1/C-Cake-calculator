12eadb1250403052ee13d74fad2c58c84446de30
mrnicola1 <115619305+mrnicola1@users.noreply.github.com>
2022-10-12 11:41:53 +0200

    Add files via upload

---
+++ Program 003,1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                Console.WriteLine("Hvor mange styk kage skal du have?:");
+                int Styk = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                Console.WriteLine("Du køber nu "+Styk);
+                Console.WriteLine("\t\t\t");
+                Console.WriteLine("\t\t\t");
+                Console.WriteLine("Styk"   +"\t\t\t"     +"Pris");
+              
+                int pris = 10;
+                int i = 0;
+                int rebat = 50;
+                int rebatpris = 8; 
+                
+                
+
+
+                while (i <= Styk)
+                {
+                   
+                    
+                    if (Styk>=rebat) {
+                        Console.WriteLine(i + "\t\t\t" + i * rebatpris + " Bitcoin");
+                        i++;
+                    }
+                    else {
+                        Console.WriteLine(i + "\t\t\t" + i * pris + " Bitcoin");
+                        i++;
+                    }
+                   
+                }
+                
+               
+                Console.ReadLine();
+
+            }
+        }
+    }
+
+
+
+}
+    
+
